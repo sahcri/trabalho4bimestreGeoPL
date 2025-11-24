9c115a591fe439db2d3c64b356dc41233e511f6b
sahcri <sarahcas75@gmail.com>
2025-11-23 23:08:29 -0300

    Add files via upload

---
+++ TrabalhoGeoHTML4b/js/script.js
@@ -0,0 +1,6 @@
+ let nome 
+ let acaoExecutada = false
+
+ nome = prompt("Digite seu nome")
+        console.log(`O nome digitado foi ${nome}`)
+        alert(`Prazer, ${nome}, sejá bem vindo(a) ao nosso trabalho interdisciplinar de geografia e PL <3`)
